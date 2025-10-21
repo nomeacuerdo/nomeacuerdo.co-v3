d1f12c13c263392bb2f6e31c2a53f922aa80c820
Nicolas Arteaga <nomeacuerdo@gmail.com>
2025-10-21 13:20:06 -0500

    Added Sanity

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { FaReact, FaNodeJs, FaGitAlt } from "react-icons/fa";
-import { SiNextdotjs, SiVuedotjs, SiTypescript, SiTailwindcss, SiShopify } from "react-icons/si";
+import { SiNextdotjs, SiVuedotjs, SiTypescript, SiTailwindcss, SiShopify, SiSanity } from "react-icons/si";
 
 export default function TechStack() {
   const techIcons = [
@@ -9,6 +9,7 @@ export default function TechStack() {
     { icon: <SiVuedotjs className="text-[#42b883] text-4xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.7)] transition" />, name: "Vue.js" },
     { icon: <SiShopify className="text-[#64f44c] text-4xl hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.7)] transition" />, name: "Shopify" },
     { icon: <SiTypescript className="text-[#3178c6] text-4xl hover:drop-shadow-[0_0_10px_rgba(255,191,0,0.7)] transition" />, name: "TypeScript" },
+    { icon: <SiSanity className="text-red-500 text-4xl hover:drop-shadow-[0_0_10px_rgba(255,191,0,0.7)] transition" />, name: "Sanity" },
     { icon: <SiTailwindcss className="text-sky-500 text-4xl hover:drop-shadow-[0_0_10px_rgba(244,63,94,0.8)] transition" />, name: "TailwindCSS" },
     { icon: <FaNodeJs className="text-[#84ba64] text-4xl hover:drop-shadow-[0_0_10px_rgba(239,68,68,0.8)] transition" />, name: "Node.js" },
     { icon: <FaGitAlt className="text-[#f05133] text-4xl hover:drop-shadow-[0_0_10px_rgba(234,179,8,0.8)] transition" />, name: "Git" },
